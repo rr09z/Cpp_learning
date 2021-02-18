cefd96a3ef9548cd1203c43d41b65574ac527a6b
rr09z <69394139+rr09z@users.noreply.github.com>
2021-02-17 23:08:57 -0500

    Add files via upload

--- Position_change.cpp
+++ Position_change.cpp
@@ -7,7 +7,9 @@ int main()
 	int number=5;
 	int arr[5];
 	int n1, n2, n3, n4, n5;
-	int t1, t2 = 0;
+	int temp = 0;
+	int start = 0;
+	int end = sizeof(arr) / sizeof(arr[0]) - 1;
 	cout << "please input the elements in the array." << endl;
 	cin >> n1;
 	arr[0] = n1;
@@ -23,15 +25,31 @@ int main()
 	{
 		cout << arr[i] << endl;
 	}
-	t1 = arr[0];
-	arr[0] = arr[4];
-	arr[4] = t1;
-	t2 = arr[1];
-	arr[1] = arr[3];
-	arr[3] = t2;
+	//temp = arr[0];
+	//arr[0] = arr[4];
+	//arr[4] = temp;
+	//temp = arr[1];
+	//arr[1] = arr[3];
+	//arr[3] = temp;
+	for (size_t i = 0; i < 5; i++)
+	{
+		if (start < end)
+		{
+			temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+		else
+		{
+			break;
+		}
+	}
 	for (size_t i = 0; i < 5; i++)
 	{
 		cout << arr[i] << endl;
 	}
+	
 
 }
